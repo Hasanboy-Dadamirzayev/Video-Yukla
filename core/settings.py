27f52dd13910deebd2f4a564305c3a8b7fbe9b86
Hasanboy-Dadamirzayev <dadamirzayevhasanboy67@gmail.com>
2025-08-09 14:18:03 +0500

    first commit

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*3!ap&8zkxof@y881i$y+s8641%(2!^vlkdr#lf)t(yq7g+=8$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 
